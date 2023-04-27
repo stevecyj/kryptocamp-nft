e90029e4e4fbbb2437763abf67824ab09de548c0
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-04-27 17:52:35 +0800

    WIP: connect wallet

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,6 +3,7 @@ import { Box, Flex, Image, Link, Spacer } from '@chakra-ui/react'
 import Facebook from './assets/social-media-icons/facebook_32x32.png'
 import Twitter from './assets/social-media-icons/twitter_32x32.png'
 import Email from './assets/social-media-icons/email_32x32.png'
+import {ConnectWallet} from "@thirdweb-dev/react";
 import { Button } from '@chakra-ui/react'
 
 
@@ -53,7 +54,7 @@ const Navbar = () => {
           fontFamily="inherit"
           padding="15px"
         >
-          Connected
+          <ConnectWallet btnTitle='連接錢包'/>
         </Box>
       </Flex>
 
